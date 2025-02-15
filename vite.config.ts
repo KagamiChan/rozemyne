c92b26de6afd482acd345b620ab502bf7e598825
かがみ <kagami@kagami.me>
2025-02-15 17:07:52 +0800

    feat: fonts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import markdown from 'vite-plugin-markdown'
+import { plugin as markdown } from 'vite-plugin-markdown'
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
