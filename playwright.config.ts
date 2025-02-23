3e4397939487197dcb8900914abf6fb350961687
かがみ <kagami@kagami.me>
2025-02-23 23:34:58 +0800

    fix: test command

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run start',
+    command: 'corepack pnpm dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
   },
