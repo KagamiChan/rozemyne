b04d2c6f6ed0242b4f105f42374a441273801747
かがみ <kagami@kagami.me>
2025-02-23 23:33:13 +0800

    chore: eslint exclude gitignore

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,15 @@
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import tseslint from 'typescript-eslint'
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
 
 export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
   tseslint.configs.stylistic,
   eslintPluginPrettierRecommended,
   {
